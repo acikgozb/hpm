0552592169a2aa32e178d23150e7f8aeedaab481
acikgozb <acikgozb@proton.me>
2025-04-28 19:39:03 +0300

    feat(main): Return appropriate ExitCode's from main
    The binary crate is updated to return appropriate `ExitCode`'s for each individual error.
    
    The `process::Error::Interrupted` is assumed to be a SIGINT signal, hence the ExitCode 130.
    
    Signed-off-by: acikgozb <acikgozb@proton.me>

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,29 @@
 use std::process::ExitCode;
 
-mod cli;
+use hpm::{cli, process};
 
 fn main() -> ExitCode {
-    match cli::run() {
+    match hpm::run() {
         Ok(_) => ExitCode::SUCCESS,
         Err(hpm_err) => {
             eprintln!("{hpm_err}");
-            ExitCode::FAILURE
+
+            if let Some(err) = hpm_err.downcast_ref::<process::Error>() {
+                return ExitCode::from(match err {
+                    process::Error::BinaryDoesNotExist(_) => 1u8,
+                    process::Error::FailedToExecProcess(_, _) => 1u8,
+                    process::Error::Exec(ecode, _) => ecode.to_owned(),
+                    process::Error::Interrupted => 130u8,
+                });
+            }
+
+            if let Some(err) = hpm_err.downcast_ref::<cli::Error>() {
+                return ExitCode::from(match err {
+                    cli::Error::FailedToWriteStdout(_) => 1u8,
+                });
+            }
+
+            ExitCode::from(2u8)
         }
     }
 }
